b5cbbdb2310fff62ee29bc8c2f652bc86a86999d
Henry Lawson <henry.lawson@foinq.com>
2017-05-24 17:50:21 +0200

    endpoint specific tools

--- configs/Poliyu/manual.sh
+++ configs/Poliyu/manual.sh
@@ -6,15 +6,28 @@ install_maven() {
   if [ ! -f $SUCCESS ]
   then
     cd /opt/user
-    wget http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
-    tar xzvf apache-maven-3.5.0-bin.tar.gz
-    rm apache-maven-3.5.0-bin.tar.gz
+    wget -O package.tar.gz http://www-us.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+    tar xzvf package.tar.gz
+    rm package.tar.gz
+    touch $SUCCESS
+  fi
+}
+
+install_endpoints_frameworks_tools() {
+  local SUCCESS="/opt/user/endpoints-framework-tools-2.0.0-beta.7-SUCCESS"
+  if [ ! -f $SUCCESS ]
+  then
+    cd /opt/user
+    wget -O package.zip http://search.maven.org/remotecontent?filepath=com/google/endpoints/endpoints-framework-tools/2.0.0-beta.7/endpoints-framework-tools-2.0.0-beta.7.zip
+    unzip package.zip
+    rm package.zip
     touch $SUCCESS
   fi
 }
 
 main() {
   install_maven
+  install_endpoints_frameworks_tools
 }
 
 main "$@"
