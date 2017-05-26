818fa4dd1e8136f6c29752aff62d629d2ad0a42a
Henry Lawson <henry.lawson@foinq.com>
2017-05-26 09:57:27 +0200

    adding autocomplete

--- configs/Poliyu/bash_profile.sh
+++ configs/Poliyu/bash_profile.sh
@@ -3,3 +3,5 @@ export PATH=$PATH:/opt/user/endpoints-framework-tools-2.0.0-beta.12/bin
 export PATH=$PATH:/opt/user/google_appengine
 export PATH=$PATH:/opt/user/clvv-fasd-4822024
 export PATH=$PATH:/opt/user/gradle-3.5/bin
+
+source /usr/lib/google-cloud-sdk/completion.bash.inc
