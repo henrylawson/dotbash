f2abeef6faf6c22977c060708816c43082d25fd1
Henry Lawson <henry.lawson@foinq.com>
2018-01-22 07:18:25 +0000

    add helm to path

--- configs/Poliyu/bash_profile.sh
+++ configs/Poliyu/bash_profile.sh
@@ -2,3 +2,4 @@ export PATH=$PATH:/opt/user/apache-maven-3.5.0/bin
 export PATH=$PATH:/opt/user/endpoints-framework-tools-2.0.0-beta.12/bin
 export PATH=$PATH:/opt/user/clvv-fasd-4822024
 export PATH=$PATH:/opt/user/gradle-3.5/bin
+export PATH=$PATH:/opt/user/helm/linux-amd64
