0d9c4f89cbccbf4a359a0f7af84222bcf525f371
Henry Lawson <henry.lawson@foinq.com>
2018-02-05 06:51:42 +0000

    installing helm

--- configs/Picolo/install.sh
+++ configs/Picolo/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-UTIL_PATH=~/utils
+UTILS_PATH=~/utils
 
 install_helm() {
   local SUCCESS="${UTILS_PATH}/helm-v2.8.0-SUCCESS"
@@ -9,7 +9,7 @@ install_helm() {
   then
     mkdir -p "${UTILS_PATH}/helm"
     cd "${UTILS_PATH}/helm"
-    wget -O package.tar.gz https://kubernetes-helm.storage.googleapis.com/helm-v2.8.0-darwin-amd64.tar.gz
+    curl -L -o package.tar.gz https://kubernetes-helm.storage.googleapis.com/helm-v2.8.0-darwin-amd64.tar.gz
     tar xzvf package.tar.gz
     rm package.tar.gz
     touch $SUCCESS
