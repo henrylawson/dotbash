309bbc3fed997f4305693bcb4b894e5d7d7860ac
Henry Lawson <henry.lawson@foinq.com>
2018-02-18 12:20:50 +0000

    cleanup

--- configs/Picolo/install.sh
+++ configs/Picolo/install.sh
@@ -4,29 +4,30 @@ set -euo pipefail
 UTILS_PATH=~/utils
 
 install_helm() {
-  local SUCCESS="${UTILS_PATH}/helm-v2.8.0-SUCCESS"
-  if [ ! -f $SUCCESS ]
+  local version="v2.8.0"
+  local success="${UTILS_PATH}/helm-${version}-SUCCESS"
+  if [ ! -f "${success}" ]
   then
     mkdir -p "${UTILS_PATH}/helm"
     cd "${UTILS_PATH}/helm"
-    curl -L -o package.tar.gz https://kubernetes-helm.storage.googleapis.com/helm-v2.8.0-darwin-amd64.tar.gz
+    curl -L -o package.tar.gz https://kubernetes-helm.storage.googleapis.com/helm-${version}-darwin-amd64.tar.gz
     tar xzvf package.tar.gz
     rm package.tar.gz
-    touch $SUCCESS
+    touch "${success}"
   fi
 }
 
 install_go() {
-  local VERSION="go1.10"
-  local SUCCESS="${UTILS_PATH}/%{VERSION}-SUCCESS"
-  if [ ! -f $SUCCESS ]
+  local version="go1.10"
+  local success="${UTILS_PATH}/${version}-SUCCESS"
+  if [ ! -f "${success}" ]
   then
     mkdir -p "${UTILS_PATH}/go"
     cd "${UTILS_PATH}/go"
-    curl -L -o package.tar.gz "https://dl.google.com/go/${VERSION}.darwin-amd64.tar.gz"
+    curl -L -o package.tar.gz "https://dl.google.com/go/${version}.darwin-amd64.tar.gz"
     tar xzvf package.tar.gz
     rm package.tar.gz
-    touch $SUCCESS
+    touch "${success}"
   fi
 }
 
