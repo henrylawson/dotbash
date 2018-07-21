809a84da4b44a7b664386d8dfd82e2bf76977a12
Henry Lawson <henry.lawson@foinq.com>
2018-07-21 17:24:51 +0100

    updated

--- configs/Prasp/install.sh
+++ configs/Prasp/install.sh
@@ -22,40 +22,40 @@ setup_raspberry_pi() {
   backup_file /usr/share/X11/xorg.conf.d/40-libinput.conf
   sudo sh -c "cat <<EOF > /usr/share/X11/xorg.conf.d/40-libinput.conf
 # Match on all types of devices but tablet devices and joysticks
-Section "InputClass"
-        Identifier "libinput pointer catchall"
-        MatchIsPointer "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
-        Option "NaturalScrolling" "true"
+Section \"InputClass\"
+        Identifier \"libinput pointer catchall\"
+        MatchIsPointer \"on\"
+        MatchDevicePath \"/dev/input/event*\"
+        Driver \"libinput\"
+        Option \"NaturalScrolling\" \"true\"
 EndSection
 
-Section "InputClass"
-        Identifier "libinput keyboard catchall"
-        MatchIsKeyboard "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
+Section \"InputClass\"
+        Identifier \"libinput keyboard catchall\"
+        MatchIsKeyboard \"on\"
+        MatchDevicePath \"/dev/input/event*\"
+        Driver \"libinput\"
 EndSection
 
-Section "InputClass"
-        Identifier "libinput touchpad catchall"
-        MatchIsTouchpad "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
+Section \"InputClass\"
+        Identifier \"libinput touchpad catchall\"
+        MatchIsTouchpad \"on\"
+        MatchDevicePath \"/dev/input/event*\"
+        Driver \"libinput\"
 EndSection
 
-Section "InputClass"
-        Identifier "libinput touchscreen catchall"
-        MatchIsTouchscreen "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
+Section \"InputClass\"
+        Identifier \"libinput touchscreen catchall\"
+        MatchIsTouchscreen \"on\"
+        MatchDevicePath \"/dev/input/event*\"
+        Driver \"libinput\"
 EndSection
 
-Section "InputClass"
-        Identifier "libinput tablet catchall"
-        MatchIsTablet "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
+Section \"InputClass\"
+        Identifier \"libinput tablet catchall\"
+        MatchIsTablet \"on\"
+        MatchDevicePath \"/dev/input/event*\"
+        Driver \"libinput\"
 EndSection
 EOF"
 
