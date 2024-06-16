5bf633ac552f8e913b85da80fd82b10146f77e12
Henry Lawson <henry.lawson@foinq.com>
2024-06-16 21:59:09 +1000

    no inputs

--- configs/prame/install.sh
+++ configs/prame/install.sh
@@ -64,7 +64,7 @@ install_google_chrome() {
 }
 
 install_1password() {
-  curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --batch --yes --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
 
   echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
 
@@ -74,7 +74,7 @@ install_1password() {
 
   sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 
-  curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --batch --yes --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
   sudo apt update
   sudo apt install 1password -y
