9d8778d1d334780fa4a0190b3157feafae15cf21
Henry Lawson <henry.lawson@foinq.com>
2024-06-16 21:30:22 +1000

    chrome

--- configs/prame/install.sh
+++ configs/prame/install.sh
@@ -55,7 +55,7 @@ EOF
 
 install_google_chrome() {
   wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
-  gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
+  sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
   echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg]  https://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
   sudo apt update
