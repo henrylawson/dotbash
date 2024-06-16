096f679547438bb5ecfe57348730e01312449467
Henry Lawson <henry.lawson@foinq.com>
2024-06-16 19:52:33 +1000

    bash

--- configs/prame/install.sh
+++ configs/prame/install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 UTILS_PATH=~/utils
 
 install_rclone() {
-    sudo -v ; curl https://rclone.org/install.sh | sudo bash
+  sudo -v ; curl https://rclone.org/install.sh | sudo bash || exit 0
 }
 
 install_rclone
