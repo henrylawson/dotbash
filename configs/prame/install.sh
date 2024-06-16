13ed77217272f6b86f5a4b56999559516c6c5216
Henry Lawson <henry.lawson@foinq.com>
2024-06-16 19:56:49 +1000

    rclone

--- configs/prame/install.sh
+++ configs/prame/install.sh
@@ -6,6 +6,8 @@ UTILS_PATH=~/utils
 install_rclone() {
   if hash rclone 2>/dev/null
   then
+    echo "rlcone already installed"
+  else
     sudo -v ; curl https://rclone.org/install.sh | sudo bash
   fi
 }
