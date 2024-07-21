9fb4e0d4353e51b65c1346157f641507e523faa0
Henry Lawson <henry.lawson@foinq.com>
2024-07-21 21:11:33 +1000

    if needed

--- configs/prame/bash_profile.sh
+++ configs/prame/bash_profile.sh
@@ -5,7 +5,9 @@ export PATH=$PATH:/usr/local/go/bin
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
 
 # ssh agent
-eval `ssh-agent` &> /dev/null
-ssh-add ~/.ssh/id_rsa &> /dev/null
-ssh-add ~/.ssh/google_compute_engine &> /dev/null
-ssh-add ~/.ssh/molotius_id_rsa &> /dev/null
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+    eval `ssh-agent` &> /dev/null
+    ssh-add ~/.ssh/id_rsa &> /dev/null
+    ssh-add ~/.ssh/google_compute_engine &> /dev/null
+    ssh-add ~/.ssh/molotius_id_rsa &> /dev/null
+fi
