46c2d83e232c7c6a7eb83b7699095ff97916b60b
Henry Lawson <henry.lawson@foinq.com>
2024-07-21 16:16:26 +1000

    remember

--- configs/prame/bash_profile.sh
+++ configs/prame/bash_profile.sh
@@ -3,3 +3,9 @@ export PATH=$PATH:/usr/local/go/bin
 
 # glcoud
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
+
+# ssh agent
+eval `ssh-agent`
+ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/google_compute_engine
+ssh-add ~/.ssh/molotius_id_rsa
