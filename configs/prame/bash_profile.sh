913cfaa48bdfee08f0eda934d6659623297ddb6c
Henry Lawson <henry.lawson@foinq.com>
2025-11-04 08:31:30 +1100

    SSH keys via 1Password

--- configs/prame/bash_profile.sh
+++ configs/prame/bash_profile.sh
@@ -7,9 +7,6 @@ export CLOUDSDK_PYTHON_SITEPACKAGES=1
 # ssh agent
 if [ -z "$SSH_AUTH_SOCK" ] ; then
     eval `ssh-agent` &> /dev/null
-    ssh-add ~/.ssh/id_rsa &> /dev/null
-    ssh-add ~/.ssh/google_compute_engine &> /dev/null
-    ssh-add ~/.ssh/molotius_id_rsa &> /dev/null
 fi
 
 # rbenv
