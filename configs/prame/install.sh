bc787b3dfb0f1492c199990bed316d3472426f5a
Henry Lawson <henry.lawson@foinq.com>
2026-05-17 09:16:14 +1000

    new key

--- configs/prame/install.sh
+++ configs/prame/install.sh
@@ -119,7 +119,7 @@ install_spotify() {
   then
     echo 'Repo exists'
   else
-    curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+    curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
     sudo apt update -y
   fi
