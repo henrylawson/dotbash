190eacb6e22764a71081026ef6844c2901a50c47
Henry Lawson <henry.lawson@foinq.com>
2026-05-24 10:01:25 +1000

    agy cli

--- configs/prame/install.sh
+++ configs/prame/install.sh
@@ -200,12 +200,19 @@ install_framework_firmware() {
 
 install_google_antigravity() {
   sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://us-central1-apt.pkg.dev/doc/repo-signing-key.gpg | \
-  sudo gpg --dearmor --yes -o /etc/apt/keyrings/antigravity-repo-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/antigravity-repo-key.gpg] https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/ antigravity-debian main" | \
-  sudo tee /etc/apt/sources.list.d/antigravity.list > /dev/null
+  curl -fsSL https://us-central1-apt.pkg.dev/doc/repo-signing-key.gpg | \
+    sudo gpg --dearmor --yes -o /etc/apt/keyrings/antigravity-repo-key.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/antigravity-repo-key.gpg] https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/ antigravity-debian main" | \
+    sudo tee /etc/apt/sources.list.d/antigravity.list > /dev/null
   sudo apt update -y
   sudo apt install -y antigravity
+
+  if hash agy 2>/dev/null
+  then
+    echo "antigravity cli already installed"
+  else
+    curl -fsSL https://antigravity.google/cli/install.sh | bash
+  fi
 }
 
 install_bun() {
